5f6b148fe7053f7a6f9ec3c1c9e95338da61e38d
Nakul Yadav <nakulydv71@gmail.com>
2025-01-30 16:51:48 +0530

    L3q3.java

--- Lab 3/L3q3.java
+++ Lab 3/L3q3.java
@@ -3,11 +3,16 @@ import java.util.*;
 class L3q3 {
     public static void moveZeroes(int[] nums) {
         int index = 0;
-        for (int num : nums) {
-            if (num != 0) nums[index++] = num;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
         }
+
         while (index < nums.length) {
-            nums[index++] = 0;
+            nums[index] = 0;
+            index++;
         }
     }
 
