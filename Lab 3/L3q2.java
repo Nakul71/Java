5a805da55dc80988016b85ca9460e364958f3d0a
Nakul Yadav <nakulydv71@gmail.com>
2025-01-30 16:51:18 +0530

    L3q2.java

--- Lab 3/L3q2.java
+++ Lab 3/L3q2.java
@@ -1,16 +1,23 @@
-
 class L3q2 {
     public static int majorityElement(int[] nums) {
         int count = 0, candidate = 0;
-        for (int num : nums) {
-            if (count == 0) candidate = num;
-            count += (num == candidate) ? 1 : -1;
+
+        for (int i = 0; i < nums.length; i++) {
+        if (count == 0) {
+            candidate = nums[i];
         }
-        return candidate;
+        if (nums[i] == candidate) {
+            count++;
+        } 
+        else {
+            count--;
+    }
+}
+return candidate;
     }
 
     public static void main(String[] args) {
         int[] nums = {2, 2, 1, 1, 1, 2, 2};
-        System.out.println(majorityElement(nums)); 
+        System.out.println("The majority element is : "+ majorityElement(nums)); 
     }
 }
