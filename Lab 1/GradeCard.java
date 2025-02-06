37666101278ddfa5783c00bc587e2c3e1cb57a1d
Nakul Yadav <nakulydv71@gmail.com>
2025-02-06 16:43:01 +0530

    Create GradeCard.java

---
+++ Lab 1/GradeCard.java
@@ -0,0 +1,12 @@
+class GradeCard {
+    public static void main(String args[]){ 
+    System.out.println("*****************************"); 
+    System.out.println("\t My Grade Card");  
+    System.out.println("Name- Nakul Yadav"); 
+    System.out.println("Sap Id- 500121882"); 
+    System.out.println("Enrollment number-R2142230114"); 
+    System.out.println("CGPA-: 7.6"); 
+    System.out.println("\tRESULT-PASS"); 
+    System.out.println("*****************************"); 
+  } 
+} 
